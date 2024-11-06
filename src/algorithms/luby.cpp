47c507bc4c0377c025f7ca5a214f01fa56df6f27
ewaldherr <waldherr49@gmail.com>
2024-11-06 11:58:57 +0100

    free state of rng after usage

--- src/algorithms/luby.cpp
+++ src/algorithms/luby.cpp
@@ -15,6 +15,7 @@ KOKKOS_FUNCTION void initializePriorities(Kokkos::View<double*> priorities) {
     Kokkos::parallel_for("init_priorities", priorities.extent(0), KOKKOS_LAMBDA(int i) {
         auto generator = random_pool.get_state();
         priorities(i) = generator.drand(0., 1.);
+        random_pool.free_state(generator);
     });
 }
 
