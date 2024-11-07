b31e1b5d612c4627dcf36b88f63aa4f83ebb5396
ewaldherr <waldherr49@gmail.com>
2024-11-07 06:42:57 +0100

    fix usage of chrono lib

--- src/algorithms/luby.cpp
+++ src/algorithms/luby.cpp
@@ -104,7 +104,7 @@ int main(int argc, char* argv[]) {
             auto start = std::chrono::high_resolution_clock::now();
             result_mis = lubysAlgorithm(xadj,adjncy);
             auto stop = std::chrono::high_resolution_clock::now();
-            auto duration = duration_cast<seconds>(stop - start);
+            auto duration = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
             std::cout << "Determined MIS in " << duration.count() << " seconds" << std::endl;
 
             writeIndependentSetToFile(result_mis,"result_mis.txt");
