d85f51f95d9b04a1e6566f528f2536198e47330c
ewaldherr <waldherr49@gmail.com>
2024-11-11 14:44:21 +0100

    fix: creation of output directory

--- src/read-write/output.cpp
+++ src/read-write/output.cpp
@@ -2,10 +2,17 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <filesystem> // C++17 or later
 
 void writeIndependentSetToFile(const Kokkos::View<int*> independentSet, const std::string &filename) {
+    // Ensure the "results" directory exists
+    std::filesystem::path dir("results");
+    if (!std::filesystem::exists(dir)) {
+        std::filesystem::create_directory(dir);
+    }
+
     // Open the output file in write mode
-    std::ofstream outputFile("/results/" + filename);
+    std::ofstream outputFile(dir / filename); // Use directory path with filename
     if (!outputFile.is_open()) {
         throw std::runtime_error("Failed to open output file.");
     }
@@ -17,7 +24,7 @@ void writeIndependentSetToFile(const Kokkos::View<int*> independentSet, const st
     // Loop through the entries in the independent set and write each entry to a new line
     for (size_t i = 0; i < h_independentSet.extent(0); ++i) {
         outputFile << h_independentSet(i) << "\n";
-        if(h_independentSet(i) == 1) size++;
+        if (h_independentSet(i) == 1) size++;
     }
     std::cout << "There are " << size << " vertices inside of the MIS" << std::endl;
 
