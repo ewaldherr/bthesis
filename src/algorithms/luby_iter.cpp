9798c2756acbfaa5608a5aaa82092098b387f8bc
ewaldherr <waldherr49@gmail.com>
2024-11-14 06:12:03 +0100

    add print for debug

--- src/algorithms/luby_iter.cpp
+++ src/algorithms/luby_iter.cpp
@@ -7,8 +7,10 @@ void checkSize(Kokkos::View<int*>& best_solution, Kokkos::View<int*>& current_so
     int size = 0;
     // Loop through the entries in the independent set and write each entry to a new line
     for (size_t i = 0; i < h_current.extent(0); ++i) {
+        std::cout << h_current(i) << " ";
         if (h_current(i) == 1) size++;
     }
+    std::cout << std::endl;
     if(size > best_size){
         best_size = size;
         best_solution = current_solution;
@@ -30,7 +32,7 @@ KOKKOS_FUNCTION void removeAtRandom(Kokkos::View<int*>& xadj , Kokkos::View<int*
     });
 }
 
-// Degree-based version of Luby's Algorithm
+// Iterative Luby's Algorithm with removing vertices from the solution 
 Kokkos::View<int*> LubyIterAlgorithm(Kokkos::View<int*> xadj, Kokkos::View<int*> adjncy, int iterations) {
     int size = 0;
     int& best_size = size;
@@ -38,6 +40,7 @@ Kokkos::View<int*> LubyIterAlgorithm(Kokkos::View<int*> xadj, Kokkos::View<int*>
     Kokkos::View<int*> best_solution("best_solution", xadj.extent(0)-1);
     auto h_current = Kokkos::create_mirror_view(current_solution);
     Kokkos::deep_copy(current_solution, -1);
+    Kokkos::deep_copy(best_solution, -1);
 
     for(int i =0; i < iterations; ++i){
         current_solution = lubysAlgorithm(xadj, adjncy, current_solution);
