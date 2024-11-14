4f6167b2430fbe51c2fe360716c775fd9498b649
ewaldherr <waldherr49@gmail.com>
2024-11-14 07:39:39 +0100

    use correct conversion method

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -30,7 +30,8 @@ int main(int argc, char* argv[]) {
 
             unsigned int seed;
             if(argc > 4){
-                seed = (unsigned int)argv[4];
+                std::string str_seed= argv[4];
+                seed = std::stoi(str_seed);
             } else{
                 seed = (unsigned int)time(NULL);
             }
