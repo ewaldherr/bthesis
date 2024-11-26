53eebccbccc0f4c862e7352aeb97f4ad6e733200
ewaldherr <waldherr49@gmail.com>
2024-11-26 08:58:21 +0100

    do 5 benchmark runs at default

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -52,7 +52,7 @@ int main(int argc, char* argv[]) {
                 Kokkos::View<int*> result_mis("mis",xadj.extent(0)-1);
                 std::cout << "Determining MIS of " << argv[1] << " with " << xadj.extent(0)-1 << " nodes and " << adjncy.extent(0)/2 << " edges using " << algo << "."<< std::endl;
 
-                for(int i = 0; i < 3; ++i){
+                for(int i = 0; i < 5; ++i){
                     // Set up degrees
                     Kokkos::View<int*> degree("degree", xadj.extent(0)-1);
                     degree = initializeDegrees(xadj);
